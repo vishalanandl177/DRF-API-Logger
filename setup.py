02ca8ed2090d93a0d197795592665c74a289472e
vishal <vishal@uniqolabel.com>
2021-02-19 02:43:44 +0530

    Rebuilding

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_long_desc():
 
 setuptools.setup(
     name="drf_api_logger",
-    version="1.0.0",
+    version="1.0.1",
     author="Vishal Anand",
     author_email="vishalanandl177@gmail.com",
     description="An API Logger for your Django Rest Framework project.",
@@ -20,7 +20,6 @@ setuptools.setup(
     install_requires=["djangorestframework>=3.7.4", "bleach>=3.1.5"],
     license='GNU General Public License v3.0',
     python_requires='>=3.5',
-    include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
