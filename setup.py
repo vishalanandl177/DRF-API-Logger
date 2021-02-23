bec330dd6c80afba6f827579fee94c6d5164cd53
vishal <vishalanandl177@gmail.com>
2021-02-23 22:50:17 +0530

    Testing readme file

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+# Copyright (c) 2020-2021 Vishal Anand
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
 import setuptools
 
 
 def get_long_desc():
-    with open("README.md", "r") as fh:
+    with open("README.rst", "r") as fh:
         long_description = fh.read()
     return long_description
 
 
 setuptools.setup(
     name="drf_api_logger",
-    version="1.0.3",
+    version="1.0.4",
     author="Vishal Anand",
     author_email="vishalanandl177@gmail.com",
     description="An API Logger for your Django Rest Framework project.",
@@ -27,7 +39,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
