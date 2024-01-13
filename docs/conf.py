3d90fc365be9ed8d13a6a4421c39a3bf5908193c
Vishal Anand <vishalanandl177@gmail.com>
2024-01-13 22:06:52 +0530

    <Vishal> Updated index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -14,3 +14,11 @@ napoleon_google_docstring = False  # Turn off googledoc strings
 napoleon_numpy_docstring = True  # Turn on numpydoc strings
 napoleon_use_ivar = True  # For maths symbology
 html_theme = 'sphinx_rtd_theme'
+
+project = u'DRF API Logger'
+copyright = u'2020, Vishal Anand'
+author = u'Vishal Anand'
+
+googleanalytics_id = 'G-BVJJKEEBVZ'
+
+pygments_style = 'sphinx'
